21c89b7c578c2fd91b771f47312e98a5030e525d
Nirbhay231 <144772839+Nirbhay231@users.noreply.github.com>
2025-03-21 01:18:17 +0530

    Commit-1
    Addition code has been added

---
+++ JavaCalculator/src/CalculatorPrograms/addition.java
@@ -0,0 +1,26 @@
+package CalculatorPrograms;
+
+import java.util.Scanner;
+
+public class addition {
+	
+	public static int add(int a, int b) 
+	{
+		int result = a+b;
+		return result;
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the first Number");
+		 int num1 = sc.nextInt();
+		 System.out.println("Enter the second Number");
+		 int num2 = sc.nextInt();
+		 int sum = add(num1,num2);
+		 System.out.println("The addition of two inputs is" +sum);
+	      sc.close(); 
+	}
+
+}
